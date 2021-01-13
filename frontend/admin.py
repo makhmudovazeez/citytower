19e2dda663c6e24735130143db6fa4c0b89df680
Makhmudov <39191427+makhmudovazeez@users.noreply.github.com>
2021-01-14 00:22:04 +0500

    admin panel

--- frontend/admin.py
+++ frontend/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import Group
+from .models import Works
+from .models import WoksImages
+from .models import Feedback
+from .models import About
+from .models import Contacts
+from .models import Services
 
-# Register your models here.
+admin.site.site_header = 'Consctuction Tower SM Admin Panel'
+admin.site.unregister(Group)
+admin.site.register(Works)
+admin.site.register(WoksImages)
+admin.site.register(Feedback)
+admin.site.register(About)
+admin.site.register(Contacts)
+admin.site.register(Services)
